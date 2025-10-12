cf9678b2f708b69e4851b0122ae40fe86c009f29
Josh L. Espinoza <jol.espinoz@gmail.com>
2025-10-11 21:42:12 -0700

    Fixed run scripts

--- run/veba/commands.classify-eukaryotic.sh
+++ run/veba/commands.classify-eukaryotic.sh
@@ -8,6 +8,6 @@ n_threads=16
 veba_database=${VEBA_DATABASE} # If you have VEBA_DATABASE environmental variable set, if not use then put path
 
 # Run
-params=" -i ../Analysis/veba_output/binning/${org_type}/ -c ../Analysis/veba_output/cluster/output/global/mags_to_slcs.tsv -o ../Analysis/veba_output/classify/${org_type}/ --n_jobs ${n_threads} --veba_database ${veba_database}"
+params=" -i ../../Analysis/veba_output/binning/${org_type}/ -c ../../Analysis/veba_output/cluster/output/global/mags_to_slcs.tsv -o ../../Analysis/veba_output/classify/${org_type}/ --n_jobs ${n_threads} --veba_database ${veba_database}"
 /usr/bin/time -v veba --module ${module} --params "${params}" 2> logs/${job_name}.e 1> logs/${job_name}.o
 
